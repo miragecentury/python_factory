fdbaa6bfe702756ff26729ab58442990048c1efa
miragecentury <victorien.vanroye@gmail.com>
2024-08-17 04:54:14 +0200

    feat(apis): :sparkles: add readiness and models

--- src/python_factory/core/api/v1/sys/health.py
+++ src/python_factory/core/api/v1/sys/health.py
@@ -4,21 +4,54 @@ API v1 sys health module.
 Provide the Get health endpoint
 """
 
+from enum import StrEnum
 from http import HTTPStatus
 
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Response
+from pydantic import BaseModel
 
 api_v1_sys_health = APIRouter(prefix="/health")
 
 
-@api_v1_sys_health.get(path="", tags=["sys"])
-def get_api_v1_sys_health() -> JSONResponse:
+class HealthStatusEnum(StrEnum):
+    """
+    Health status enum.
+    """
+
+    HEALTHY = "healthy"
+    UNHEALTHY = "unhealthy"
+
+
+class HealthResponseModel(BaseModel, Response):
+    """
+    Health response schema.
+    """
+
+    status: HealthStatusEnum
+
+
+@api_v1_sys_health.get(
+    path="",
+    tags=["sys"],
+    response_model=HealthResponseModel,
+    responses={
+        HTTPStatus.OK.value: {
+            "model": HealthResponseModel,
+            "description": "Health status.",
+        },
+        HTTPStatus.INTERNAL_SERVER_ERROR.value: {
+            "model": HealthResponseModel,
+            "description": "Internal server error.",
+        },
+    },
+)
+def get_api_v1_sys_health(response: Response) -> HealthResponseModel:
     """
     Get the health of the system.
     Args:
-        request (Request): The request object.
+        response (Response): The response object.
     Returns:
-        Response: The response object.
+        HealthResponse: The health status.
     """
-    return JSONResponse(status_code=HTTPStatus.OK.value, content={"status": "OK"})
+    response.status_code = HTTPStatus.OK
+    return HealthResponseModel(status=HealthStatusEnum.HEALTHY)
